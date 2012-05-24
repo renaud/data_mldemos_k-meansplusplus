dcc1e33b0164ef3e681dfee40bfc3256974c9d20
Renaud Richardet <renaud@apache.org>
2012-05-24 16:06:29 +0200

    first commit

---
+++ plot.m
@@ -0,0 +1,21 @@
+% first import the 2 datasets using File > Import Data and rename them km and kmp
+% then paste the code below in Matlab
+
+legendString = {['k-means (n=200'],	['k-means++ (n=200)']};
+
+xBins = 7:0.5:40;          					% Bin positions will be 0.5 apart
+[n_km, x_km] = hist(km, xBins, 'k');       	% Histogram of km
+[n_kmp, x_kmp] = hist(kmp, xBins, 'r');    	% Histogram of kmp
+
+figure
+hold on
+
+stairs(x_km, n_km/sum(n_km), 'k');          % km is black
+stairs(x_kmp, n_kmp/sum(n_kmp), 'r');
+
+xlabel('RSS');
+ylabel('Frequency');
+legend(legendString,  'Location', 'NorthEast');
+title('Comparison of k-means and k-means++')
+
+hold off
